fa39e5c596579c821f2d7be7b63cb9f40644175c
Velidan <wellninedotnet@gmail.com>
2015-11-23 22:50:00 +0200

    add RUN handlers

--- engine/public/js/angular/app.js
+++ engine/public/js/angular/app.js
@@ -6,7 +6,22 @@
         'view-segment'
     ]).constant('config', {
         mediaPath : '/media'
-    });
+    }).run(['$rootScope', '$location', function($rootScope, $location){
+        /* before fire app we check if we are authorized. If not - prevent
+         * template loading and redirect to auth form */
+        $rootScope.$on("$routeChangeStart", function(event, next, current) {
+
+            var isAuthorized = UTIL.getCookie('isAuthorized') || false;
+
+            if (next.originalPath !== '/' && !isAuthorized) {
+                event.preventDefault();
+                $location.url('/');
+            } else {
+                UTIL.clearHTMLSignInIdentif();
+            }
+
+        });
+    }]);
 
 
 }(window));
